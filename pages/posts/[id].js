5dea0f30abe49ea4dc67988a29ee0507749f27c0
alerafart <maalejandrarafart@gmail.com>
2022-09-10 18:10:43 +0200

    render markdown library remark remark html installed

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -11,6 +11,8 @@ export default function Post({ postData }) {
         {postData.id}
         <br />
         {postData.date}
+        <br />
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
       </Layout>
     );
   }
@@ -24,7 +26,7 @@ export default function Post({ postData }) {
   }
   
   export async function getStaticProps({ params }) {
-    const postData = getPostData(params.id);
+    const postData = await getPostData(params.id);
     return {
       props: {
         postData,
